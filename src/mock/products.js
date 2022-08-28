e378742c6e921eab65ebe622e6a520b3400b821f
lucas-perata <contacto.perata@gmail.com>
2022-08-28 13:03:10 -0300

    Mock de productos

--- src/mock/products.js
+++ src/mock/products.js
@@ -1,29 +1,29 @@
  export const products = [
     {
         id: 1, 
-        author: 'Gene Wolfe',
-        title: 'New Sun',
+        author: 'Roberto Bolaño',
+        title: 'Los detectives salvajes',
         price: 100,
         stock: 5,
         genero: 'Fantasía',
-        img: 'https://res.cloudinary.com/dbjgowpyd/image/upload/v1661647002/51WH5G2dopL_km1jya.jpg'
+        img: 'https://res.cloudinary.com/dbjgowpyd/image/upload/v1661701019/e3e20ebca7405b9d3e36ca378527bfb97aff2db5_vsqdsb.jpg'
     },
     {
         id: 2, 
-        author: 'Gene Wolfe',
-        title: 'New Moon',
+        author: 'Thomas Pynchon',
+        title: 'The crying of lot 49',
         price: 150,
         stock: 4,
         genero: 'Fantasía',
-        img: 'https://res.cloudinary.com/dbjgowpyd/image/upload/v1661647002/51WH5G2dopL_km1jya.jpg'
+        img: 'https://res.cloudinary.com/dbjgowpyd/image/upload/v1661701028/9780099532613_tib0tz.jpg'
     }, 
     {
         id: 3, 
-        author: 'Gene Wolfe',
-        title: 'New Earth',
+        author: 'William Shakespeare',
+        title: 'Hamlet',
         price: 170,
         stock: 2,
         genero: 'Fantasía',
-        img: 'https://res.cloudinary.com/dbjgowpyd/image/upload/v1661647002/51WH5G2dopL_km1jya.jpg'
+        img: 'https://res.cloudinary.com/dbjgowpyd/image/upload/v1661701031/hamlet-271_ncrdal.jpg'
     }
  ]
