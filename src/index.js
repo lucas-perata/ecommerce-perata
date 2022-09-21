d07ce8738b08f77fa4eda3737bc36bb919f46537
lucas-perata <contacto.perata@gmail.com>
2022-09-20 23:43:43 -0300

    Agregado Firebase

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCipYAiPp5rK3UFGHY-mkQWE0sLLTZ0ad4",
+  authDomain: "ecommerce-books-5307c.firebaseapp.com",
+  projectId: "ecommerce-books-5307c",
+  storageBucket: "ecommerce-books-5307c.appspot.com",
+  messagingSenderId: "205260474230",
+  appId: "1:205260474230:web:51eaf6a3915940bc8edc57",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
