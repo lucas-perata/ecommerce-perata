48feaeb62b47af6c663196c8982feef936fb019a
lucas-perata <contacto.perata@gmail.com>
2022-10-04 12:53:45 -0300

    Fix footer styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,35 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
 
 export default function Footer() {
   return (
-    <footer className=" bottom-0 left-0 z-20 p-4 w-full bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+    <footer className="top z-20 p-4 w-full bg-white border-t border-gray-200 shadow shadow-cyan-500/50 md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
         © 2022{" "}
         <a href="https://flowbite.com/" class="hover:underline">
-          Flowbite™
+          Lucas Perata™
         </a>
-        . All Rights Reserved.
       </span>
-      <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+      <ul className="flex flex-wrap items-center mt-3 gap-5 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6 ">
-            About
-          </a>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6">
-            Privacy Policy
-          </a>
-        </li>
-        <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6">
-            Licensing
-          </a>
-        </li>
-        <li>
-          <a href="#" class="hover:underline">
-            Contact
-          </a>
+          <Link to="/about-us">Sobre nosotros</Link>
         </li>
       </ul>
     </footer>
