5f0346e501ab834632395332c38ed2be19af41ea
lucas-perata <contacto.perata@gmail.com>
2022-10-10 20:23:10 -0300

    Productos sin stock no son mostrados

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -9,44 +9,47 @@ const ItemList = (props) => {
   return (
     <>
       {props.items.map((item) => {
-        if (
-          (params.categoryName === "nuevo" &&
-            item.data().category === "nuevo") ||
-          (params.categoryName === "usado" && item.data().category === "usado")
-        ) {
-          return (
-            <div
-              id="load"
-              className="w-80 p-5 bg-white shadow rounded transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
-            >
-              <div key={item.data().id}> </div>
-              <Item
-                title={item.data().title}
-                author={item.data().author}
-                price={item.data().price}
-                img={item.data().img}
-                id={item.data().id}
-              />
-            </div>
-          );
-        } else if (!params.categoryName) {
-          return (
-            <div
-              id="load"
-              className="w-80 p-5 bg-white shadow rounded transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
-            >
-              <div key={item.data().id}> </div>
-              <Item
-                title={item.data().title}
-                author={item.data().author}
-                price={item.data().price}
-                img={item.data().img}
-                id={item.data().id}
-              />
-            </div>
-          );
-        } else {
-          return "";
+        if (item.data().stock !== 0) {
+          if (
+            (params.categoryName === "nuevo" &&
+              item.data().category === "nuevo") ||
+            (params.categoryName === "usado" &&
+              item.data().category === "usado")
+          ) {
+            return (
+              <div
+                id="load"
+                className="w-80 p-5 bg-white shadow rounded transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+              >
+                <div key={item.data().id}> </div>
+                <Item
+                  title={item.data().title}
+                  author={item.data().author}
+                  price={item.data().price}
+                  img={item.data().img}
+                  id={item.data().id}
+                />
+              </div>
+            );
+          } else if (!params.categoryName) {
+            return (
+              <div
+                id="load"
+                className="w-80 p-5 bg-white shadow rounded transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+              >
+                <div key={item.data().id}> </div>
+                <Item
+                  title={item.data().title}
+                  author={item.data().author}
+                  price={item.data().price}
+                  img={item.data().img}
+                  id={item.data().id}
+                />
+              </div>
+            );
+          } else {
+            return "";
+          }
         }
       })}
     </>
